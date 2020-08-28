be169bc2fbe91c3c4bc1983bc81f0b329b1e7bc3
qinjialei <qinjialei@foxmail.com>
2020-08-28 15:31:36 +0800

    Update

--- bundle.js
+++ bundle.js
@@ -1,12 +1,22 @@
 const fs = require('fs')
+const path = require('path')
 const parser = require('@babel/parser')
+const traverse = require('@babel/traverse').default
 
 const getModuleInfo = file => {
   const body = fs.readFileSync(file, 'utf-8')
   const ast = parser.parse(body, {
     sourceType: 'module'
   })
-  console.log('ast: ', ast.program.body);
+  const deps = {}
+  traverse(ast, {
+    ImportDeclaration({ node }) {
+      const dirname = path.dirname(file)
+      const abspath = './' + path.join(dirname, node.source.value)
+      deps[node.source.value] = abspath
+    }
+  })
+  console.log('deps 的值是：', deps);
 }
 
 getModuleInfo('./src/index.js')
